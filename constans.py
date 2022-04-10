81503f9246b710280e8367556e1e915adced8dd8
axelsid <100712659+axelsid@users.noreply.github.com>
2022-04-10 17:54:50 +0200

    Add files via upload

---
+++ constans.py
@@ -0,0 +1,12 @@
+HIREACTIONS = ["cześć", "witaj", "hej", "dzień dobry"]
+HOWAREREACTIONS = ["jak się masz", "jak tam", "co słychać", "jak u ciebie"]
+NAMEREACTIONS = ["jak masz na imię", "jak się nazywasz", "kim jesteś", "jak do ciebie mówić"]
+MATHREACTIONS = ["oblicz", "policz", "ile to", "ile się równa"]
+EXITREACTIONS = ["wyłącz się", "koniec pracy", "żegnaj", "do widzenia", "to koniec"]
+
+HILABELS = ["greet", "greet", "greet", "greet"]
+HOWARELABELS = ["hay", "hay", "hay", "hay"]
+NAMELABELS = ["name", "name", "name", "name"]
+MATHRLABELS = ["math", "math", "math", "math"]
+EXITLABELS = ["exit", "exit", "exit", "exit", "exit"]
+
